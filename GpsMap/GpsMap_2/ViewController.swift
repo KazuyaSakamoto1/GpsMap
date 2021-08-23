8ddb7e67309d454f42cbc3c51d48ca09aa9d9939
hiroto0927 <83230897+hiroto0927@users.noreply.github.com>
2021-08-23 18:44:09 +0900

    拡大・縮小ボタンの追加

--- GpsMap/GpsMap_2/ViewController.swift
+++ GpsMap/GpsMap_2/ViewController.swift
@@ -7,10 +7,45 @@
 
 import UIKit
 import CoreLocation
+import MapKit //地図表示のプログラム
 
 class ViewController: UIViewController , CLLocationManagerDelegate{
-
+    var myLock = NSLock()
+    let goldenRatio = 1.618
+    
+    @IBOutlet var mapView: MKMapView!
     var locationManager : CLLocationManager!
+    
+    
+    //拡大のボタン
+    @IBAction func ZoomIn(_ sender: Any) {
+        print("[DBG]clickZoomin")
+        myLock.lock()
+        if (0.005 < mapView.region.span.latitudeDelta / goldenRatio) {
+                print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
+                var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+                regionSpan.latitudeDelta = mapView.region.span.latitudeDelta / goldenRatio
+                mapView.region.span = regionSpan
+                print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
+        }
+        myLock.unlock()
+    }
+    
+    
+    //縮小のボタン
+    @IBAction func ZoomOut(_ sender: Any) {
+        print("[DBG]clickZoomout")
+        myLock.lock()
+        if (mapView.region.span.latitudeDelta * goldenRatio < 150.0) {
+                print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
+                var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+                regionSpan.latitudeDelta = mapView.region.span.latitudeDelta * goldenRatio
+        regionSpan.latitudeDelta = mapView.region.span.longitudeDelta * goldenRatio
+                mapView.region.span = regionSpan
+                print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
+            }
+        myLock.unlock()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +56,8 @@ class ViewController: UIViewController , CLLocationManagerDelegate{
         
         locationManager.startUpdatingLocation()
         locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]){
@@ -30,7 +67,11 @@ class ViewController: UIViewController , CLLocationManagerDelegate{
         print("[DBG]longitude : " + longitude)
         print("[DBG]longitude : " + latitude)
         
+        myLock.lock()
+        mapView.setCenter((locations.last?.coordinate)!, animated: true) // 現在の位置情報を中心に表示（更新）
+        myLock.unlock()
     }
+    
 
 
 }
