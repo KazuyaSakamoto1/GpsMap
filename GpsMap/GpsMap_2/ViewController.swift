5a7f1833bff2ee74fd28c6b7fcfd8a9d8a2c8677
hiroto0927 <83230897+hiroto0927@users.noreply.github.com>
2021-08-25 17:57:14 +0900

    画面の初期設定

--- GpsMap/GpsMap_2/ViewController.swift
+++ GpsMap/GpsMap_2/ViewController.swift
@@ -6,25 +6,25 @@
 //
 
 import UIKit
-import CoreLocation
+import CoreLocation //位置情報を取得するためのフレームワーク
 import MapKit //地図表示のプログラム
 
 class ViewController: UIViewController , CLLocationManagerDelegate{
     var myLock = NSLock()
-    let goldenRatio = 1.618
+    let up_goldenRatio = 1.618
+    let down_goldenRatio = 2.0
     
     @IBOutlet var mapView: MKMapView!
     var locationManager : CLLocationManager!
-    
     
     //拡大のボタン
     @IBAction func ZoomIn(_ sender: Any) {
         print("[DBG]clickZoomin")
         myLock.lock()
-        if (0.005 < mapView.region.span.latitudeDelta / goldenRatio) {
+        if (0.0001 < mapView.region.span.latitudeDelta / up_goldenRatio) {
                 print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
                 var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
-                regionSpan.latitudeDelta = mapView.region.span.latitudeDelta / goldenRatio
+                regionSpan.latitudeDelta = mapView.region.span.latitudeDelta / up_goldenRatio
                 mapView.region.span = regionSpan
                 print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
         }
@@ -35,34 +35,37 @@ class ViewController: UIViewController , CLLocationManagerDelegate{
     //縮小のボタン
     @IBAction func ZoomOut(_ sender: Any) {
         print("[DBG]clickZoomout")
+        print(mapView.region.span.latitudeDelta)
         myLock.lock()
-        if (mapView.region.span.latitudeDelta * goldenRatio < 150.0) {
+        if (mapView.region.span.latitudeDelta * down_goldenRatio < 150.0) {
                 print("[DBG]latitudeDelta-1 : " + mapView.region.span.latitudeDelta.description)
                 var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
-                regionSpan.latitudeDelta = mapView.region.span.latitudeDelta * goldenRatio
-        regionSpan.latitudeDelta = mapView.region.span.longitudeDelta * goldenRatio
+                regionSpan.latitudeDelta = mapView.region.span.latitudeDelta * down_goldenRatio
+                regionSpan.latitudeDelta = mapView.region.span.longitudeDelta * down_goldenRatio
                 mapView.region.span = regionSpan
                 print("[DBG]latitudeDelta-2 : " + mapView.region.span.latitudeDelta.description)
             }
         myLock.unlock()
     }
     
+    //位置情報の取得
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         locationManager = CLLocationManager(); //変数を初期化
-        locationManager.delegate = self
+        locationManager.delegate = self // delegateとしてself(自インスタンス)を設定
         
-        locationManager.startUpdatingLocation()
-        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation() //GPSの使用を開始する
+        locationManager.requestWhenInUseAuthorization()// 位置情報取得の許可を得る
         mapView.showsUserLocation = true
+        initMap()
         
     }
     
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]){
-        let longitude = (locations.last?.coordinate.longitude.description)!
-        let latitude = (locations.last?.coordinate.latitude.description)!
+        let longitude = (locations.last?.coordinate.longitude.description)! //軽度
+        let latitude = (locations.last?.coordinate.latitude.description)!   //緯度
 
         print("[DBG]longitude : " + longitude)
         print("[DBG]longitude : " + latitude)
@@ -71,6 +74,19 @@ class ViewController: UIViewController , CLLocationManagerDelegate{
         mapView.setCenter((locations.last?.coordinate)!, animated: true) // 現在の位置情報を中心に表示（更新）
         myLock.unlock()
     }
+    
+    func initMap() {
+            // 縮尺を設定
+            var region:MKCoordinateRegion = mapView.region
+        region.span.latitudeDelta = 0.005
+        region.span.longitudeDelta = 0.005
+            mapView.setRegion(region,animated:true)
+
+            // 現在位置表示の有効化
+            mapView.showsUserLocation = true
+            // 現在位置設定（デバイスの動きとしてこの時の一回だけ中心位置が現在位置で更新される）
+            mapView.userTrackingMode = .followWithHeading
+        }
     
 
 
