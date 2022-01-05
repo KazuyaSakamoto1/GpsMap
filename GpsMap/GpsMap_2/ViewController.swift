fd805b3bbf63adcdfb6d8dd83cf41e3bdd4c5e9b
hiroto0927 <83230897+hiroto0927@users.noreply.github.com>
2022-01-05 17:30:15 +0900

    Update: 追跡対象ファイルの変更と設定ファイルの変更

--- GpsMap/GpsMap_2/ViewController.swift
+++ GpsMap/GpsMap_2/ViewController.swift
@@ -338,7 +338,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDe
             
             if self.regionFlag == true {
                 let preLocation = self.step.steps[self.stepCount - 1]
-                let message = "\(preLocation.instructions)です。その先、\(nextLocation.instructions)　です。"
+                let message = "\(preLocation.instructions)です。その先、\(nextLocation.distance)メートル先\(nextLocation.instructions)　です。"
                 print("領域外に出る：\(message)")
                 print(self.stepCount)
                 let speechUtterance = AVSpeechUtterance(string: message)
@@ -443,7 +443,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDe
     }
     
     @IBAction func settingsButtonAction(_ sender: Any) {
-        let settingsViewController = self.storyboard?.instantiateViewController(withIdentifier: "SettingsViewController") as! SettingsViewController
+        let settingsViewController = self.storyboard?.instantiateViewController(withIdentifier: "settingsViewController") as! SettingsViewController
         self.present(settingsViewController, animated: true, completion: nil)
     }
     
